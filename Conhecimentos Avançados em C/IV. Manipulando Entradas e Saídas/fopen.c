a70092e52cbda8d4f000e50fbcb84366ce471d50
Erick Bryan Cubas <thecubas@outlook.com>
2023-05-16 00:13:29 -0300

    commit

--- Conhecimentos Avançados em C/IV. Manipulando Entradas e Saídas/fopen.c
+++ Conhecimentos Avançados em C/IV. Manipulando Entradas e Saídas/fopen.c
@@ -13,4 +13,16 @@ int main (void){
         printf("\n Não encontrei o arquivo!\n");
         exit (EXIT_FAILURE);
     }
+    double soma = 0.0;
+    int n = 0;
+    while (true) {
+        int x;
+        int k = fscanf (entrada, "%d", &x);
+        if (k != 1) break;
+        soma += x;
+        n += 1;       
+    }
+    fclose (entrada);
+    printf("A média dos números %.2f\n", soma/n);
+    return EXIT_SUCCESS;
 }
